2d07d8665dd69a830e33e8b728678958431dca5c
Vlada-ks <122997495+Vlada-ks@users.noreply.github.com>
2024-01-11 20:57:06 +0400

    1.1.4/2

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,19 +1,14 @@
 package jm.task.core.jdbc.util;
-
-import java.lang.reflect.InvocationTargetException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class Util {
-// реализуйте настройку соеденения с БД
 
 private static final String URL = "jdbc:mysql://localhost:3306/test";
 private static final String USERNAME = "roots";
 private static final String PASSWORD = "roots";
 public static Connection connection;
-public static Statement statement;
 
     private Util() {
     }
